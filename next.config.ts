7455f7724d5d7ef9ad370ce93fa6f98f25a5ce8e
Shreyas <more3118@gmail.com>
2025-02-11 14:38:30 -0600

    Added Domain

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ['shrecodes.com', 'www.shrecodes.com'],
+  },
 };
 
 export default nextConfig;
